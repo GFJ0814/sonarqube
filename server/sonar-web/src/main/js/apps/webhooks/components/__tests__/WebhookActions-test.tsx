7d6d906037da0a84cea05672b318b2b2a4b650f5
Grégoire Aubert <gregoire.aubert@sonarsource.com>
2018-02-02 11:34:03 +0100

    SONAR-10345 Add webhooks management actions
    * SONAR-10345 Add the webhooks create/update form
    * SONAR-10345 Add the webhooks delete action
    * SONAR-10345 Add fields validation on webhook create page

--- server/sonar-web/src/main/js/apps/webhooks/components/__tests__/WebhookActions-test.tsx
+++ server/sonar-web/src/main/js/apps/webhooks/components/__tests__/WebhookActions-test.tsx
@@ -19,27 +19,47 @@
  */
 import * as React from 'react';
 import { shallow } from 'enzyme';
-import WebhooksList from '../WebhooksList';
+import WebhookActions from '../WebhookActions';
+import { click } from '../../../../helpers/testUtils';
 
-const webhooks = [
-  { key: '1', name: 'my webhook', url: 'http://webhook.target' },
-  { key: '2', name: 'jenkins webhook', url: 'http://jenkins.target' }
-];
+const webhook = { key: '1', name: 'foo', url: 'http://foo.bar' };
 
-it('should correctly render empty webhook list', () => {
-  expect(getWrapper({ webhooks: [] })).toMatchSnapshot();
+it('should render correctly', () => {
+  expect(getWrapper()).toMatchSnapshot();
 });
 
-it('should correctly render the webhooks', () => {
-  expect(getWrapper()).toMatchSnapshot();
+it('should display the update webhook form', () => {
+  const onUpdate = jest.fn(() => Promise.resolve());
+  const wrapper = getWrapper({ onUpdate });
+  click(wrapper.find('.js-webhook-update'));
+  expect(wrapper.find('CreateWebhookForm').exists()).toBeTruthy();
+  wrapper.find('CreateWebhookForm').prop<Function>('onDone')({
+    name: webhook.name,
+    url: webhook.url
+  });
+  expect(onUpdate).lastCalledWith(webhook);
+});
+
+it('should display the delete webhook form', () => {
+  const onDelete = jest.fn(() => Promise.resolve());
+  const wrapper = getWrapper({ onDelete });
+  click(
+    wrapper
+      .find('ConfirmButton')
+      .dive()
+      .find('.js-webhook-delete')
+  );
+  expect(wrapper.find('ConfirmButton').exists()).toBeTruthy();
+  wrapper.find('ConfirmButton').prop<Function>('onConfirm')();
+  expect(onDelete).lastCalledWith(webhook.key);
 });
 
 function getWrapper(props = {}) {
   return shallow(
-    <WebhooksList
+    <WebhookActions
       onDelete={jest.fn(() => Promise.resolve())}
       onUpdate={jest.fn(() => Promise.resolve())}
-      webhooks={webhooks}
+      webhook={webhook}
       {...props}
     />
   );
